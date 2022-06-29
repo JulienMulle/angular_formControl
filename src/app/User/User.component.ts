3708cfa82632b21d112bbae5d9fa74bf6871c5ba
Julien <julienmulle@gmail.com>
2022-06-29 13:36:27 +0200

    ajout de l'adresse

--- src/app/User/User.component.ts
+++ src/app/User/User.component.ts
@@ -9,11 +9,17 @@ import { User } from '../models/User';
 })
 export class UserComponent implements OnInit {
 
-  user: User = new User("","","");
+  user: User = new User("","","",0,"",0,"");
   
   UserName = new FormControl('');
   Email = new FormControl('');
   Password = new FormControl('');
+  StreetNumber = new FormControl(0);
+  Street = new FormControl('');
+  CityZip = new FormControl(0);
+  City = new FormControl('');
+
+
 
   constructor() { }
 
@@ -24,7 +30,11 @@ export class UserComponent implements OnInit {
   onSubmit(): void{
    this.user.UserName = this.UserName.value,
     this.user.Email= this.Email.value,
-    this.user.Password =this.Password.value
+    this.user.Password= this.Password.value,
+    this.user.StreetNumber = this.StreetNumber.value ,
+    this.user.Street = this.Street.value,
+    this.user.CityZip = this.CityZip.value,
+    this.user.City = this.City.value
     console.log(this.user)
    
   }
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -3,6 +3,11 @@ export class User {
         public UserName: string | null,
         public Email: string | null,
         public Password: string | null,
+        public StreetNumber: number | null,
+        public Street: string | null,
+        public CityZip: number | null,
+        public City: string | null,
     )
     {}
+    
 }
